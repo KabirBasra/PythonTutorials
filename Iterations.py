4bf21c71b182646a59cd6c918747a2fd3e91f39b
Kabir Basra <>
2024-08-13 09:36:16 +0100

    Adding iteration examples

--- Iterations.py
+++ Iterations.py
@@ -1,3 +1,22 @@
 #Testing iteration loops
+#Pre-Conditioned Iteration
+    #Prints "Hello World" twice
 for i in range(2):
     print("Hello World")
+
+
+ #Single Embedded PreConditioned Iteration
+    #Prints value 1 x3, 2x3 then 3 x3
+x = 0
+for i in range(3):
+    x += 1
+    for j in range(3):
+        print(x)
+
+    #Prints value 1 then 2 then 3 x3
+x = 0
+for i in range(3):
+    x = 0
+    for j in range(3):
+        x += 1
+        print(x)
